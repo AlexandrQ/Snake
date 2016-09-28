0e0e16d7fd0cddbbc6bf44c53bed84057f9385b3
Admin <Admin>
2016-09-28 21:41:00 +0300

    Исправил функцию установки позиции курсора

--- SnakeProject/SnakeProject/Source.cpp
+++ SnakeProject/SnakeProject/Source.cpp
@@ -3,17 +3,18 @@
 
 using namespace std;
 
-void gotoxy(int x, int y)			//устанавливаем курсор в указанные координаты
+void gotoxy(int x, int y, char z)			//устанавливаем курсор в указанные координаты
 {
 	COORD coord;
 	coord.X = x;
 	coord.Y = y;
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+	cout << z << endl;
 }
 
 int main() {
-	gotoxy(3, 3);
-	cout << "*" << endl;
+	gotoxy(3, 3, '*');
+	
 
 	system("PAUSE");
 	return 0;
